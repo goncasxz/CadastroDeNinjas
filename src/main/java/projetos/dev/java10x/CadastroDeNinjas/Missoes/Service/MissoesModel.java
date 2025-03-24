586f36fc328010dce3bb23b7d9cbbcbfeacd2869
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-03-24 12:25:52 -0300

    feat: Implementa Lombok nas classes para abstração e correção de redundancia

--- src/main/java/projetos/dev/java10x/CadastroDeNinjas/Missoes/Service/MissoesModel.java
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/Missoes/Service/MissoesModel.java
@@ -1,12 +1,18 @@
 package projetos.dev.java10x.CadastroDeNinjas.Missoes.Service;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import projetos.dev.java10x.CadastroDeNinjas.Ninjas.Service.NinjaModel;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -19,45 +25,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, String rank, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.nome = nome;
-        this.rank = rank;
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
--- src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaModel.java
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaModel.java
@@ -1,12 +1,18 @@
 package projetos.dev.java10x.CadastroDeNinjas.Ninjas.Service;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import projetos.dev.java10x.CadastroDeNinjas.Missoes.Service.MissoesModel;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -21,36 +27,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //foreign key
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
