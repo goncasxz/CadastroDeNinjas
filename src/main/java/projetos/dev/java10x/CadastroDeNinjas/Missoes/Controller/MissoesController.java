04f14d1fa4acf59b0c14a8f97e7a61aea79ef523
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-04-07 12:13:33 -0300

    feat: Cria as rotas do missoes controller com mock

---
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -0,0 +1,30 @@
+package projetos.dev.java10x.CadastroDeNinjas.Missoes.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "missoes listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "missao alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "missao deletada com sucesso";
+    }
+
+
+}
