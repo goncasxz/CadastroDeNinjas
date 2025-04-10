9623fd98d48d34ded0f3c5edff575d017b861889
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-04-10 12:58:00 -0300

    feat: Adiciona service e controller para listagem de ninjas

--- src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -1,9 +1,20 @@
 package projetos.dev.java10x.CadastroDeNinjas.Ninjas.Controller;
 import org.springframework.web.bind.annotation.*;
+import projetos.dev.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
+import projetos.dev.java10x.CadastroDeNinjas.Ninjas.Service.NinjaService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasVindas")
     public String boasVindas() {
@@ -17,15 +28,15 @@ public class NinjaController {
     }
 
     // TODO: Procurar Ninjas por ID
-    @GetMapping("/todosID")
-    public String mostrarNinjasPorId() {
-        return "Mostrar todos os ninjas por ID";
+    @GetMapping("/listar/{id}")
+    public Optional<NinjaModel> mostrarNinjasPorId(@PathVariable Long id) {
+        return ninjaService.listarNinjasId(id);
     }
 
     // TODO: Mostrar todos os Ninjas
     @GetMapping("/listar")
-    public String mostrarNinjas() {
-        return "Mostrar todos os ninjas";
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
     }
 
     // TODO: Alterar dados dos Ninjas
--- src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaService.java
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/Ninjas/Service/NinjaService.java
@@ -1,4 +1,28 @@
 package projetos.dev.java10x.CadastroDeNinjas.Ninjas.Service;
 
+import org.springframework.stereotype.Service;
+import projetos.dev.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
+import projetos.dev.java10x.CadastroDeNinjas.Ninjas.Repository.NinjaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    //TODO: Listar todos os ninjas
+
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
+
+    public Optional<NinjaModel> listarNinjasId(Long id) {
+        return ninjaRepository.findById(id);
+    }
 }
