c8f6b1ab2b8dbfbc27eb195c072bd525bb76f4dc
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-04-24 15:39:42 -0300

    chore: Adiciona Configuração de metodo PATCH e outros para ser enviado pelo formulario HTTP

---
+++ src/main/java/projetos/dev/java10x/CadastroDeNinjas/config/WebConfig.java
@@ -0,0 +1,13 @@
+package projetos.dev.java10x.CadastroDeNinjas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
